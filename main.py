2e885c916ce96c85b2f1754d1e649f47b37e97cc
brandonr734 <119473372+brandonr734@users.noreply.github.com>
2023-02-11 22:27:47 -0500

    Added buying/selling functionality
    To-do list:
    -Error checking for text inputs, (ex. entered bid values don’t contain characters)
    -Add yes/no popup for buy button
    -Check if the user has enough money to buy/bid
    -Check if the value the user is bidding is higher than the current highest bid
    -Currently can only display 9 items on the buy page
    -Add confirmation messages when user bids/buys/puts an item up for sale
    -Display item overview window with yes/no confirmation before user puts an item up for sale
    -Close popup bid window after the user submits the bid
    -Clear sell menu input fields after user submits item
    -Make UI nicer/more organized
    -Add functionality for deducting money when auction ends if user has the highest bid, or could just subtract it when the bid is placed for now.
    -Add “get more money” button
    -Sort items in buy page by timer (lowest time → highest time)
    -Disable buttons while they are not visible
    -User enters time as minutes and is converted to seconds in back-end

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import Screen
 from kivy.uix.button import ButtonBehavior
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.core.window import Window
-import datetime
+from datetime import datetime, timedelta, time
+from kivy.uix.popup import Popup
+from kivy.uix.floatlayout import FloatLayout
+from decimal import Decimal
 
 
 # User Object Class
@@ -22,16 +25,17 @@ class Bid:
         self.userId = userID
         self.bidValue = bidValue
 
+# Popup Window Class
+class Popups(FloatLayout):
+    pass
 
 # Item Object Class
 class Item:
-    def __init__(self, itemID, name, image, desc, sellerID, endDate):
-        self.itemID = itemID
+    def __init__(self, name, time, price, buy_price):
         self.name = name
-        self.image = image
-        self.desc = desc
-        self.sellerID = sellerID
-        self.endDate = endDate
+        self.time = time
+        self.price = price
+        self.buy_price = buy_price
 
 
 # Class for the home screen
@@ -42,6 +46,9 @@ class HomeScreen(Screen):
 
 # Class for the buy screen
 class BuyScreen(Screen):
+    balance = 1000.00
+    def balance_to_string(self):
+        return "${:.2f}".format(self.balance)
     pass
 
 
@@ -56,10 +63,23 @@ class ImageButton(ButtonBehavior, Image):
 
 
 GUI = Builder.load_file("main.kv")
-
+items = []
+item1 = Item("Shoes", 1800, 25.00, 50.00)
+item2 = Item("Phone", 3600, 500.00, 750.00)
+item3 = Item("TV", 10, 600.00, 800.00)
+item4 = Item("Laptop", 2700, 300.00, 400.00)
+item5 = Item("Shirt", 900, 15.00, 35.00)
+items.append(item1)
+items.append(item2)
+items.append(item3)
+items.append(item4)
+items.append(item5)
 
 # Class of the application
 class CloudMarket(App):
+    curr_item = -1
+    balance = 1000.00
+
     # Builds the UI
     def build(self):
         Window.size = (1000, 575)
@@ -89,20 +109,115 @@ class CloudMarket(App):
             screen_manager.transition.direction = 'right'
         screen_manager.current = screen_name
 
+    # Tasks performed once when the app is launched
+    def on_start(self):
+        self.root.ids.sell_screen.ids.dropdown.dismiss()
+        # name, time, price, buy_price
+        item_ids = self.root.ids.buy_screen.ids
+        for i in range(len(items)):
+            item = getattr(item_ids, "item_name_" + str(i+1))
+            item.text = items[i].name
+            temp_time = items[i].time
+            temp = datetime.min + timedelta(minutes=temp_time)
+            t = time(temp.hour, temp.minute)
+            item.opacity = 1
+            item = getattr(item_ids, "item_time_" + str(i+1))
+            item.text = str(t)
+            item.opacity = 1
+            item = getattr(item_ids, "item_price_" + str(i+1))
+            item.text = "${:.2f}".format(items[i].price)
+            item.opacity = 1
+            item = getattr(item_ids, "item_buy_" + str(i+1))
+            item.text = "${:.2f}".format(items[i].buy_price)
+            item.opacity = 1
+            item = getattr(item_ids, "bid_button_" + str(i+1))
+            item.opacity = 1
+            item = getattr(item_ids, "buy_button_" + str(i+1))
+            item.opacity = 1
+        for i in range(len(items), 9):
+            item = getattr(item_ids, "item_name_" + str(i+1))
+            item.opacity = 0
+            item = getattr(item_ids, "item_time_" + str(i+1))
+            item.opacity = 0
+            item = getattr(item_ids, "item_price_" + str(i+1))
+            item.opacity = 0
+            item = getattr(item_ids, "item_buy_" + str(i+1))
+            item.opacity = 0
+            item = getattr(item_ids, "bid_button_" + str(i+1))
+            item.opacity = 0
+            item = getattr(item_ids, "buy_button_" + str(i+1))
+            item.opacity = 0
+
     # Tasks performed once per second
     def update(self, dt):
-        current_time = datetime.datetime.now()
-        end_date_string = "2023-02-04 12:00:00"
-        end_date = datetime.datetime.strptime(end_date_string, "%Y-%m-%d %H:%M:%S")
-        time_remaining = end_date - current_time
-        total_seconds = time_remaining.total_seconds()
-        days, remainder = divmod(total_seconds, 86400)
-        hours, remainder = divmod(remainder, 3600)
-        minutes, seconds = divmod(remainder, 60)
-        time_string = "{:.0f}:{:.0f}:{:.0f}:{:02.0f}".format(days, hours, minutes, seconds)
-        HomeScreen.counter = "Time Remaining: " + time_string
-#       self.root.ids.home_screen.ids.title_label.text = str(HomeScreen.counter)
+        item_ids = self.root.ids.buy_screen.ids
+        for i in range(len(items)):
+            item = getattr(item_ids, "item_name_" + str(i+1))
+            item.text = items[i].name
+            item.opacity = 1
+            item = getattr(item_ids, "item_price_" + str(i+1))
+            item.text = "${:.2f}".format(items[i].price)
+            item.opacity = 1
+            item = getattr(item_ids, "item_buy_" + str(i+1))
+            item.text = "${:.2f}".format(items[i].buy_price)
+            item.opacity = 1
+            item = getattr(item_ids, "item_time_" + str(i+1))
+            item.opacity = 1
+            item = getattr(item_ids, "bid_button_" + str(i+1))
+            item.opacity = 1
+            item = getattr(item_ids, "buy_button_" + str(i+1))
+            item.opacity = 1
 
+        for i in range(len(items), 9):
+            item = getattr(item_ids, "item_name_" + str(i+1))
+            item.opacity = 0
+            item = getattr(item_ids, "item_time_" + str(i+1))
+            item.opacity = 0
+            item = getattr(item_ids, "item_price_" + str(i+1))
+            item.opacity = 0
+            item = getattr(item_ids, "item_buy_" + str(i+1))
+            item.opacity = 0
+            item = getattr(item_ids, "bid_button_" + str(i+1))
+            item.opacity = 0
+            item = getattr(item_ids, "buy_button_" + str(i+1))
+            item.opacity = 0
+        for i in range(len(items)-1, -1, -1):
+            seconds = items[i].time
+            hours = seconds // 3600
+            minutes = (seconds % 3600) // 60
+            timer_seconds = seconds % 60
+            t = "{:02d}:{:02d}:{:02d}".format(hours, minutes, timer_seconds)
+            item = getattr(item_ids, "item_time_" + str(i+1))
+            item.text = t
+            items[i].time = items[i].time - 1
+            if items[i].time <= -1:
+                del items[i]
+
+    def show_popup(self, item_num):
+        show = Popups()
+        self.curr_item = item_num
+        popupWindow = Popup(title="Bid Window", content=show, size_hint=(None, None), size=(300, 170))
+        popupWindow.open()
+
+    def submit_bid(self, bid_value):
+        bid_value = Decimal(bid_value)
+        items[self.curr_item].price = bid_value
+
+    def submit_item(self, name, ptime, min_bid, buy_price):
+        ptime = int(ptime)
+        min_bid = Decimal(min_bid)
+        buy_price = Decimal(buy_price)
+        print(ptime)
+        new_item = Item(name, ptime, min_bid, buy_price)
+        items.append(new_item)
+        for i in range(len(items)):
+            print(items[i].name)
+
+    def buy_item(self, item_num):
+        self.balance = float(self.balance) - float(items[item_num].buy_price)
+        BuyScreen.balance = self.balance
+        self.root.ids.buy_screen.ids.balance_label.text = "${:.2f}".format(self.balance)
+        del items[item_num]
 
 # -MAIN---------------------------------------------------------------------------------------------------
 CloudMarket().run()

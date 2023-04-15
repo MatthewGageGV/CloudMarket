64688a60b27576ae552c7f809294cd1222bf4df3
brandonr734 <119473372+brandonr734@users.noreply.github.com>
2023-04-14 23:21:00 -0400

    Delete trackscreen.py

--- trackscreen.py
+++
@@ -1,9 +0,0 @@
-from kivy.uix.screenmanager import Screen
-
-class TrackScreen(Screen):
-    """Creates the trackscreen"""
-    balance = 1000.00
-
-    def balance_to_string(self):
-        """converts balance to dollar format"""
-        return "${:.2f}".format(self.balance)
